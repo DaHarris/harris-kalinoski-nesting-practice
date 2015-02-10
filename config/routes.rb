e074d2b4918e05f2b588b10ec46140393f4d222d
Daniel Harris <bobth3bum@gmail.com>
2015-02-10 14:09:13 -0700

    add index with company table

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resources :companies do
+    resources :locations
+    resources :products do
+      resources :reviews
+    end
+  end
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'companies#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
