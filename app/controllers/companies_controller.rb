ee2dcbc06622646c68a354a668debc4d967fba6e
Daniel Harris <bobth3bum@gmail.com>
2015-02-10 14:31:20 -0700

    added show page for companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,4 +4,40 @@ class CompaniesController < ApplicationController
     @companies = Company.all
   end
 
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    @company.update(company_params)
+    redirect_to company_path(@company)
+  end
+
+  def create
+    @company = Company.new(company_params)
+    @company.save
+    redirect_to company_path(@company)
+  end
+
+  def destroy
+    @company = Company.find(params[:id])
+    @company.destroy
+    redirect_to companies_path
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :suffix, :catch_phrase, :email, :start_date, :industry)
+  end
+
 end
